8888b7aebd0f9cc62237e725aba0fb281a66f622
Francis-Gurr <francis.gurr@gmail.com>
2024-12-31 17:54:50 +0000

    Add exception to balance assertion for closing statements

--- validate_transactions/utils/balance_assertions.py
+++ validate_transactions/utils/balance_assertions.py
@@ -16,11 +16,11 @@ def validate_balance_assertion(entry):
         ]
 
     valid_statement_date = entry.date + relativedelta(months=+1) - datetime.timedelta(days=1)
-    if not entry.meta["statement"].endswith(f"{valid_statement_date}.pdf"):
+    if not entry.meta["statement"].endswith(f"{valid_statement_date}.pdf") and not entry.meta["statement"].endswith("_closing-statement.pdf"):
         errors.append(
             BalanceAssertionError(
                 entry.meta,
-                f"Statement file must be date one month from the balance assertion date ({valid_statement_date}.pdf)",
+                f"Statement file must be date one month from the balance assertion date ({valid_statement_date}.pdf), or a closing statement (_closing-statement.pdf)",
                 entry,
             )
         )
